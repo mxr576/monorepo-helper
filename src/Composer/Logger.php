2cd774a7c9ae6a52d884550ab6b7fbadd710a1a4
Dezső BICZÓ <mxr576@gmail.com>
2019-07-25 13:15:21 +0200

    Introducing new composer-logger package

--- src/Composer/Logger.php
+++ src/Composer/Logger.php
@@ -25,7 +25,6 @@ namespace Pronovix\MonorepoHelper\Composer;
 
 use Composer\IO\IOInterface;
 use Psr\Log\AbstractLogger;
-use Psr\Log\LogLevel;
 
 /**
  * PSR-3 logger wrapper around IOInterface.
@@ -33,64 +32,23 @@ use Psr\Log\LogLevel;
 final class Logger extends AbstractLogger
 {
     /**
-     * @var \Composer\IO\IOInterface
+     * @var \Pronovix\ComposerLogger\Logger
      */
-    private $io;
+    private $decorated;
 
     /**
-     * Logger constructor.
-     *
-     * @param \Composer\IO\IOInterface $io
+     * @inheritDoc
      */
     public function __construct(IOInterface $io)
     {
-        $this->io = $io;
+        $this->decorated = new \Pronovix\ComposerLogger\Logger('Monorepo Helper', $io);
     }
 
     /**
      * @inheritDoc
      */
-    public function log($level, $message, array $context = []): void
+    public function log($level, $message, array $context = [])
     {
-        switch ($level) {
-            case LogLevel::ERROR:
-            case LogLevel::CRITICAL:
-            case LogLevel::ALERT:
-            case LogLevel::EMERGENCY:
-                $this->io->writeError([$this->buildMessage($message, $context)]);
-                break;
-            case LogLevel::DEBUG:
-                if ($this->io->isDebug()) {
-                    $this->io->write([$this->buildMessage($message, $context)]);
-                }
-                break;
-            default:
-                if ($this->io->isVerbose()) {
-                    $this->io->write([$this->buildMessage($message, $context)]);
-                }
-        }
-    }
-
-    /**
-     * Interpolates context values into the message placeholders and prefixes the message with the plugin's name.
-     *
-     * @param string $message
-     * @param array $context
-     *
-     * @return string
-     *
-     * @see https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md#12-message
-     */
-    private function buildMessage(string $message, array $context): string
-    {
-        $replace = [];
-        foreach ($context as $key => $val) {
-            // Check that the value can be casted to string.
-            if (!is_array($val) && (!is_object($val) || method_exists($val, '__toString'))) {
-                $replace['{' . $key . '}'] = $val;
-            }
-        }
-
-        return 'Monorepo Helper: ' . strtr($message, $replace);
+        $this->decorated->log($level, $message, $context);
     }
 }
